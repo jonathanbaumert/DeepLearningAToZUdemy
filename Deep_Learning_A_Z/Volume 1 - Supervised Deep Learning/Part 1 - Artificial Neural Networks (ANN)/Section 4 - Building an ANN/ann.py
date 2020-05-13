368fdd7933202c338006946a31587cb699407912
Jonathan Baumert <jonathanbaumert@yahoo.com>
2020-05-12 21:21:57 -0700

    import encoders and encode the country data field

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -21,3 +21,20 @@ dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 Y = dataset.iloc[:, 13].values
 
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+onehotencoder = ColumnTransformer([('one_hot_encoder',OneHotEncoder(),[1])],remainder = 'passthrough')
+
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:,1]) #encode the country column
+
+X = onehotencoder.fit_transform(X)
+
+# Encoding the Dependend Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(Y)
+
+
+
